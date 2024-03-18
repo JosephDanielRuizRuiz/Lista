884486430f8e13bd5d32760a001514dd4d853558
Jose Ruiz <joseph.ruiz@uniminuto.edu.co>
2024-03-17 22:45:09 -0500

    Modificando tabla

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListatipificacionComponent } from './listatipificacion/listatipificacion.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    redirectTo:'/listatipificacion',
+    pathMatch:'full'
+  },
+  {
+    path:'listatipificacion',
+    component: ListatipificacionComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
